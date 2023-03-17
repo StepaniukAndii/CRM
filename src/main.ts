316be00ce3a5fa4a810a207d35f7d1e4b4111a41
andy.qa <andy.qa@prof-it.bz>
2023-03-17 09:15:31 +0200

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('CRM')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('crm')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
